cf58969fffe0a29276eb97a20d1680c0b5e9b50d
bigails <abigailotengmarfo@gmail.com>
2023-04-14 22:57:37 -0700

    Corrected erros

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 
 	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fd == -1)
+	if (fl == -1)
 	return (-1);
 
 	if (text_content != NULL)
